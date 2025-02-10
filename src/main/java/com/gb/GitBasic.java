523eb9d33e3d26245df1265ceeecd1d3d445da8b
Kams <kdasgupta@umassd.edu>
2025-02-09 20:52:19 -0500

    commit the recent change

--- src/main/java/com/gb/GitBasic.java
+++ src/main/java/com/gb/GitBasic.java
@@ -3,6 +3,6 @@ package com.gb;
 public class GitBasic {
     public static void main(String[] args)
     {
-        System.out.println("Let us get familiarized with GIT");
+        System.out.println("Let us get familiarized with GIT with basic git commands!");
     }
 }
