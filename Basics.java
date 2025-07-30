9e2f736e3b779966a28e92c192451c2a0612f416
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-07-30 01:24:24 -0400

    Add files via upload

---
+++ Basics.java
@@ -0,0 +1,20 @@
+package com;
+class Basics
+{
+  public void printName(String name)
+    {
+      System.out.println("Hello " + name);
+    }
+  public static void main(String[] args)
+  {
+    System.out.println("Let us learn Git !!!!");
+
+    Basics b = new Basics();
+    b.printName("Kams");
+
+ 
+  }
+
+ 
+}
+
