cdb8a8485bbbadd88a4c415734c8dcc12f275db3
Kams <159199110+dasguptakamalika@users.noreply.github.com>
2025-12-31 00:06:40 -0500

    Update GitBasic.java

--- src/main/java/com/gb/GitBasic.java
+++ src/main/java/com/gb/GitBasic.java
@@ -3,6 +3,6 @@ package com.gb;
 public class GitBasic {
     public static void main(String[] args)
     {
-        System.out.println("Let us get familiarized with GIT with basic git commands!");
+        System.out.println("Let us get familiarized with GIT with basic git commands!"); 
     }
 }
